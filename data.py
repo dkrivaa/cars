0149cc975c04afa1f443730d573d7da8ae231883
dkrivaa <dannykrivaa@hotmail.com>
2023-12-16 20:17:56 +0200

    Check cars from Transport Ministry Databases

--- data.py
+++ data.py
@@ -29,3 +29,24 @@ def data_license():
             print(data['result']['records'])
             info.append(data['result']['records'])
 
+    print(info)
+
+
+def similar():
+    resource_id1 = "053cea08-09bc-40ec-8f7a-156f0677aff3"
+    # Specify the filter value for the degem_nm column
+    degem_nm = 117.343
+    # Construct the URL with the filter
+    api_url = f"https://data.gov.il/api/action/datastore_search?resource_id={resource_id1}&filters=%7B%22degem_nm%22%3A%20{degem_nm}%7D"
+
+    # Make the HTTP request
+    response = requests.get(api_url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        print(response.status_code)
+        # Parse the response JSON
+        data = json.loads(response.text)
+        print(data)
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import data
 
-data.data_license()
+# data.data_license()
+data.similar()
